12bb7cd945c50b48e811016184b246ad573b8db3
Luka de Vrij <lukadevrij@gmail.com>
2023-10-19 11:58:36 +0200

    bord

--- Bord.cs
+++ Bord.cs
@@ -4,30 +4,38 @@ public abstract class Bord
 {
     public Kuiltje[,] Kuiltjes;
     public List<ThuisKuiltje> ThuisKuiltjes; // Moet miss Kuiltjes type zijn
+    public int rijen;
+    public int kuiltjes_per_rij;
 }
 
 public class MankalaV1Bord : Bord
 {
 
-    public MankalaV1Bord()
+    public MankalaV1Bord(int rijen, int kuiltjes_per_rij)
     {
         // Maak bord met bepaalde hvh kuiltjes
-        this.Kuiltjes = new Kuiltje[2, 7];
+        this.rijen = rijen;
+        this.kuiltjes_per_rij = kuiltjes_per_rij;
+        this.Kuiltjes = new Kuiltje[rijen, kuiltjes_per_rij];
         this.ThuisKuiltjes = new List<ThuisKuiltje>();
         fillBord();
     }
     private void fillBord()
     { // V1 heeft in elk Kuiltje 4 steentjes
 
+        
         for (int kolom = 0; kolom < Kuiltjes.GetLength(0); kolom++)
         {
             for (int rij = 0; rij < Kuiltjes.GetLength(1); rij++)
             {
                 if (Kuiltjes[kolom, rij].GetType() == typeof(ThuisKuiltje)) // https://stackoverflow.com/questions/3561202/check-if-instance-is-of-a-type
                 {
+                    Kuiltjes[kolom, rij] = new ThuisKuiltje(0, kolom + 1);
                     continue;
                 }
-                Kuiltjes[kolom, rij].steentjes = 4; 
+
+                Kuiltjes[kolom, rij] = new BordKuiltje(4, kolom + 1);
+
             }
         }
     }
@@ -39,7 +47,7 @@ public class MankalaV1Bord : Bord
         // volgorde van mankala (arab style)
         StringBuilder sb = new StringBuilder();
         
-        for (int rij = 0; rij < rijen; rij++) // TODO rijen definieren
+        for (int rij = 0; rij < Kuiltjes.GetLength(1); rij++)
         {
             for (int kolom = 0; kolom < Kuiltjes.GetLength(0); kolom++)
             {
